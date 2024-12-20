6ff33d439f1400ea989d9115004058f4f58bab7d
ZAHOOR FATIMA <160278589+zahoor303@users.noreply.github.com>
2024-12-20 22:57:32 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,23 @@ import Footer from "./Homepage/footer";
 import HeroSection from "./Homepage/hero";
 import RecommendationCar from "./Homepage/recommand";
 import CategoryPage from "./catageroyPage/page";
-import CarDetailPage from "./carDeatailpage/hero";
-import PaymentPage from "./paymentpage/payment";
-import Dashboard  from "./Dashboardpage/dash";
+import CarDetailPage from "./carDeatailpage/page";
+import PaymentPage from "./paymentpage/page";
+import Dashboard  from "./Dashboardpage/page";
+
 
 
 
 export default function Home() {
   return (
    <div> 
-    
     <Navbar/>
    <HeroSection/>
    <CataloguePage/>
    <RecommendationCar />
    <Footer/>
-   <CategoryPage/>
-   <CarDetailPage/>
-   <PaymentPage/>
-   <Dashboard/>
    </div>
   );
 }
+
+
